aca06da0ce9b91763df212ae6ac3e906f7d06697
Akash Likhitkar <johndoe@example.com>
2024-03-07 21:37:22 +0530

    to-do list

--- script.js
+++ script.js
@@ -22,11 +22,9 @@ listContainer.addEventListener('click',function(e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked")
         saveData()
-
     }
     else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
-        // console.log(e.target.parentElement)
         saveData()
     }
 },false)
